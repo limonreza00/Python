49fe9ccbd7368c8da39afa7c5d6476675f242c79
limonreza00 <limonreza3.1416@gmail.com>
2025-08-13 22:02:26 +0600

    import second function

--- class001_basic/iron_man.py
+++ class001_basic/iron_man.py
@@ -1,3 +1,5 @@
-from hello import greet
+from hello import greet,realName
 
-greet("Tony Stark is Iron Man")
+greet("This is Iron Man") #first function calling from another file
+
+realName("My real name is Tony Stark") #second function calling from another file

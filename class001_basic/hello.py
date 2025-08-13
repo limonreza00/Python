ad7715a8b87b267391696e79db015393fdc4ac62
limonreza00 <limonreza3.1416@gmail.com>
2025-08-13 21:59:26 +0600

    add realName function

--- class001_basic/hello.py
+++ class001_basic/hello.py
@@ -1,7 +1,14 @@
 
 print("hello python") # This is a simple print statement
 
+
+#function 
 def greet(name):
     print(name)
 
-greet("Iron Man")
+greet("Iron Man") #first function calling 
+
+def realName(real_name):
+    print(real_name)
+
+realName("Tony Stark") #second function calling
